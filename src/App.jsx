94dde187408a33590970717a7c217f36a169935b
Developer-Rizwan-Zahid <rizwanzahid091@gmail.com>
2025-07-16 14:19:00 -0700

    Fix case-sensitive import path for Vercel build

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import Hero from './components/Hero'
-import Service from './components/service'
-import Destination from './components/Destination'
-import Trip from './components/Trip'
-import Company from './components/Company'
-import Subscibe from './components/Subscibe'
-import Footer from './components/Footer'
+import Service from './components/service.jsx'
+import Destination from './components/Destination.jsx'
+import Trip from './components/Trip.jsx'
+import Company from './components/Company.jsx'
+import Subscibe from './components/Subscibe.jsx'
+import Footer from './components/Footer.jsx'
 
 const App = () => {
   return (
